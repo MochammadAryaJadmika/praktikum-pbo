4f87d3c839e583b22e6fe00da3ccfa5421847583
Mochammad Arya Jadmika <100180173+MochammadAryaJadmika@users.noreply.github.com>
2023-03-05 15:22:16 +0700

    Update OperasiTitik.java

--- Praktikum 2/OperasiTitik.java
+++ Praktikum 2/OperasiTitik.java
@@ -5,24 +5,43 @@
  Deskripsi	:Program kelas Operasi Titik
 **/
 
-class OperasiTitik {
-	private void refleksiSumbuX(Titik titik) {
+/********LATIHAN***********/
+class OperasiTitik1 {
+	public void refleksiSumbuX(Titik titik) {
 		double ordinat = titik.getOrdinat();
 		ordinat = ordinat * -1;
 		titik.setOrdinat(ordinat);
 	}
 	
-	private void refleksiSumbuY(Titik titik) {
+	public void refleksiSumbuY(Titik titik) {
 		double absis = titik.getAbsis();
 		absis = absis * -1;
 		titik.setAbsis(absis);
 	}
-	
-	public void refleksiX(Titik titik) {
-		refleksiSumbuX(titik);
+}
+
+/********TUGAS***********/
+class OperasiTitik {
+	private void refleksiSumbuX(Titik t1) {
+		double ordinat = t1.getOrdinat();
+		ordinat = ordinat * -1;
+		t1.setOrdinat(ordinat);
 	}
 	
-	public void refleksiY(Titik titik) {
-		refleksiSumbuY(titik);
+	private void refleksiSumbuY(Titik t1) {
+		double absis = t1.getAbsis();
+		absis = absis * -1;
+		t1.setAbsis(absis);
+	}
+	public Titik refleksiX(Titik t1) {
+		Titik T2 = new Titik(t1.getAbsis(), t1.getOrdinat()); 
+		refleksiSumbuX(T2);
+		return T2;
+	}
+	
+	public Titik refleksiY(Titik t1) {
+		Titik T2 = new Titik(t1.getAbsis(), t1.getOrdinat());
+		refleksiSumbuY(T2);
+		return T2;
 	}
 }
