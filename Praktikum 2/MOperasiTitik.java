f1e0cee1002aebcab32486e77ff3602b683e10f5
Mochammad Arya Jadmika <100180173+MochammadAryaJadmika@users.noreply.github.com>
2023-03-05 15:21:04 +0700

    Update MOperasiTitik.java

--- Praktikum 2/MOperasiTitik.java
+++ Praktikum 2/MOperasiTitik.java
@@ -6,20 +6,20 @@
 **/
 
 class MOperasiTitik{
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 	
 		Titik t1 = new Titik(4.0, 4.0);
 		OperasiTitik op = new OperasiTitik();
 		
 		System.out.println("Titik("+ t1.getAbsis()+", "+t1.getOrdinat() +")");
 		
-		op.refleksiX(t1);
+		Titik T2 = op.refleksiX(t1);
 		System.out.println("\nTitik setelah refleksi sumbu X");
-		System.out.println("Titik("+ t1.getAbsis()+", "+t1.getOrdinat() +")");
+		System.out.println("Titik("+ T2.getAbsis()+", "+T2.getOrdinat() +")");
 		
-		op.refleksiY(t1);
+		Titik T3 = op.refleksiY(t1);
 		System.out.println("\nTitik setelah refleksi sumbu Y");
-		System.out.println("Titik("+ t1.getAbsis()+", "+t1.getOrdinat() +")");
+		System.out.println("Titik("+ T3.getAbsis()+", "+T3.getOrdinat() +")");
 
 	}
 }
