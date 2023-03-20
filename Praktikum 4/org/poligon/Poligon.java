e64e2ba102251b0723ddfdbe0b5a9fc14dca7a39
Mochammad Arya Jadmika <100180173+MochammadAryaJadmika@users.noreply.github.com>
2023-03-20 18:47:45 +0700

    Add files via upload

---
+++ Praktikum 4/org/poligon/Poligon.java
@@ -0,0 +1,16 @@
+/**
+ Nama file	:Poligon.java
+ Tanggal	:8 Maret 2023
+ Penulis	:Mochammad Arya Jadmika / 24060121130085
+ Deskripsi	:Representasi dasar dari objek poligon (segi banyak)
+**/
+
+package org.poligon;
+
+public class Poligon{
+	protected int jumlahSisi;
+	
+	public int getJumlahSisi(){
+		return this.jumlahSisi;
+	}
+}
