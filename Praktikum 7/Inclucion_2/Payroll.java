1d35e1c505e256667568002ff00f74854dfc1733
Mochammad Arya Jadmika <100180173+MochammadAryaJadmika@users.noreply.github.com>
2023-05-06 15:11:32 +0700

    Update Payroll.java

--- Praktikum 7/Inclucion_2/Payroll.java
+++ Praktikum 7/Inclucion_2/Payroll.java
@@ -2,7 +2,7 @@
  Nama file	:Payroll.java
  Tanggal	:3 Mei 2023
  Penulis	:Mochammad Arya Jadmika / 24060121130085
- Deskripsi	:Kelas yang digunakan untuk mencetak gaji pegawai yang mana bagian dari kelas pegawai..
+ Deskripsi	:Kelas yang digunakan untuk mencetak gaji pegawai yang mana bagian dari kelas pegawai.
 **/
 
 public class Payroll{
